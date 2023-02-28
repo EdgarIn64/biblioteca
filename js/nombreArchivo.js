465617ba22162ec3579e619b6817bc5a7a2cb1e7
EdgarIn64 <edgar_coriaSixtyFour@hotmail.com>
2023-02-28 10:41:21 -0600

    areas

--- js/nombreArchivo.js
+++ js/nombreArchivo.js
@@ -1,7 +1,6 @@
-let archivo = document.querySelector('#archivo');
-
-archivo.addEventListener('change', () => {
-	document.querySelector('#nombreArchivo').innerText = archivo.files[0].name;
+function subirArchivo() {
+	let archivo = document.getElementById('archivo');
+	document.getElementById('nombreArchivo').innerText = archivo.files[0].name;
 	if(archivo.files[0].size > 31000000) {
 		document.getElementById('advertenciaOculta').style.display = 'block';
 		btnSubir.disabled = true;
@@ -10,11 +9,11 @@ archivo.addEventListener('change', () => {
 		document.getElementById('advertenciaOculta').style.display = 'none';
 		btnSubir.disabled = false;
 	}
-});
+}
 
 function subirIcono() {
-	icono = document.getElementById('icono');
-	btnSubir = document.getElementById('btnSubir');
+	let icono = document.getElementById('icono');
+	let btnSubir = document.getElementById('btnSubir');
 	document.getElementById('nombreArchivo').innerText = icono.files[0].name;
 	if(icono.files[0].size > 510000) {
 		document.getElementById('advertenciaOculta').style.display = 'block';
